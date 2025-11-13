730cae5f65f228130ea18de2d03e3e063b60d4c4
Flávio Ghiraldeli <ghiraldeli26@gmail.com>
2025-11-13 10:51:23 -0300

    React app Alura

---
+++ tecboard/src/App.jsx
@@ -0,0 +1,32 @@
+/** @format */
+
+import "./App.css";
+
+//no react, componentes sao FUNÇÕES
+
+function FormularioDeEvento() {
+  return (
+    <form className='form-evento'>
+      <h2>Preencha para criar um evento:</h2>
+      <fieldset>
+        <label htmlFor='nome'>Qual o nome do evento? </label>
+        <input type='text' id='nome'></input>
+      </fieldset>
+    </form>
+  );
+}
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src='/logo.png' alt=''></img>
+      </header>
+      <section>
+        <img Src='/banner.png' alt=''></img>
+      </section>
+    </main>
+  );
+}
+
+export default App;
