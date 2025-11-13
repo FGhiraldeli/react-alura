e170fd2e201b034e05fa8e11d96d0086ffc6fab1
Flávio Ghiraldeli <ghiraldeli26@gmail.com>
2025-11-13 11:44:02 -0300

    Adding functions and props

--- tecboard/src/App.jsx
+++ tecboard/src/App.jsx
@@ -4,14 +4,38 @@ import "./App.css";
 
 //no react, componentes sao FUNÇÕES
 
+// props é um OBJETO
+// props.chidren
+function TituloFormulario(props) {
+  return <h2>{props.children}</h2>;
+}
+
+function CampoDeFormulario({ children }) {
+  return <fieldset>{children}</fieldset>;
+}
+
+function Label({ children, htmlFor }) {
+  return <label htmlFor={htmlFor}>{children}</label>;
+}
+
+function CampoDeEntrada({ props }) {
+  return <input {...props} />;
+}
+
 function FormularioDeEvento() {
   return (
     <form className='form-evento'>
       <h2>Preencha para criar um evento:</h2>
-      <fieldset>
+      <CampoDeFormulario>
+        <Label>Preencha para criar um evento:</Label>
         <label htmlFor='nome'>Qual o nome do evento? </label>
-        <input type='text' id='nome'></input>
-      </fieldset>
+        <input
+          type='text'
+          id='nome'
+          placeholder='Summer dev hits'
+          name='nomeEvento'
+        />
+      </CampoDeFormulario>
     </form>
   );
 }
